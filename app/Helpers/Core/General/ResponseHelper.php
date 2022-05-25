e576f9044f9a7892ea5112215b89daa58360742c
jalal <jalalannan4@gmail.com>
2022-05-25 11:10:04 +0300

    Update ResponseHelper Class

---
+++ app/Helpers/Core/General/ResponseHelper.php
@@ -0,0 +1,56 @@
+<?php
+
+
+namespace App\Helpers\Core\General;
+
+
+class ResponseHelper
+{
+    public function createdResponse($name, $data = [])
+    {
+        return array_merge([
+            'status' => true,
+            'message' => trans('messages.created_response', [
+                'name' => __($name)
+            ]),
+        ], $data);
+    }
+    public function loginResponse($name, $data = [])
+    {
+        return array_merge([
+            'status' => true,
+            'message' => trans('messages.login_response', [
+                'name' => __($name)
+            ]),
+        ], $data);
+    }
+
+    public function updatedResponse($name, $data = [])
+    {
+        return array_merge([
+            'status' => true,
+            'message' => trans('default.updated_response', [
+                'name' => __t($name)
+            ]),
+        ], $data);
+    }
+
+    public function deletedResponse($name, $data = [])
+    {
+        return array_merge([
+            'status' => true,
+            'message' => trans('default.deleted_response', [
+                'name' => __t($name)
+            ]),
+        ], $data);
+    }
+
+    public function failedResponse($data = [])
+    {
+        return array_merge([
+            'status' => false,
+            'message' => __('messages.failed_response')
+        ], $data);
+    }
+
+}
