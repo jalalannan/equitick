f1ace8e382c403b629f391943364ee602521bda2
jalal <jalalannan4@gmail.com>
2022-05-25 11:12:17 +0300

    Add trans messages for responses

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'created_response' => ':name has been created successfully',
+    'failed_response' => 'Something Went Wrong Please Try again later',
+    'login_response' => ':name has login successfully',
+];
