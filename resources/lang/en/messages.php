f3df48ebcdccbaa5f1026e5230d6311207bf94ef
jalal <jalalannan4@gmail.com>
2022-05-26 14:50:04 +0300

    add new trans response

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -4,4 +4,5 @@ return [
     'created_response' => ':name has been created successfully',
     'failed_response' => 'Something Went Wrong Please Try again later',
     'login_response' => ':name has login successfully',
+    'return_response' => ':model has returned successfully',
 ];
